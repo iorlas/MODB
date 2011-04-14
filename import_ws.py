4eb096170f3e5334c366c5649f7ee0ec66017ecc
Den Tom <denis.tomilin@gmail.com>
2011-04-14 17:08:44 +0400

    import_ws fixed

--- import_ws.py
+++ import_ws.py
@@ -4,7 +4,7 @@ def rel(*x):
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
 #you can use -v flag for verbose output
-verbose = len(sys.argv) > 1 and sys.argv[1] == '-v'
+verbose = len(sys.argv) > 2 and sys.argv[2] == '-v'
 
 #flags for mysqldump command
 mysqldump_params = [
@@ -24,15 +24,17 @@ mysql_mangos_scripts = 'realm1_scripts'
 mysql_mangos_realm = 'mangos_realmd'
 """
 
-if len(sys.argv) > 1 and sys.argv[1] == '-h':
-    print """This file imports WORLD and SCRIPTS databases.
-    Why one separated script for only this two databases?
-    Its can be imprted any time by any server w/o migration.
-    This databases are untouchable for server and cannot keep something, what you are need to keep.
+if (len(sys.argv) > 1 and sys.argv[1] == '-h') or len(sys.argv) < 2:
+    print """python dump.py [db info file name]|-h <-v>
+This file imports WORLD and SCRIPTS databases.
+Why one separated script for only this two databases?
+Its can be imprted any time by any server w/o migration.
+This databases are untouchable for server and cannot keep something, what you are need to keep.
     """
-    print "Are you new for this? At first, you need to create a config file. Name it db_info.py. After that, copy and paste this:\n"
+    print "Are you new for this? At first, you need to create a config file. Name it db_info.py. After that, copy and paste this:"
     print demo_config
-    print "\nAnd now edit it as you need and run me!"
+    print "And now edit it as you need and run me with it's name as argument!"
+    exit(1)
 
 #helpful func for verbose output
 def dprint(s,verbose_s=None):
@@ -45,14 +47,14 @@ def dprint(s,verbose_s=None):
 
 #check for file
 try:
-    db_info = __import__('db_info')
+    db_info = __import__(sys.argv[1].replace('.py', ''))
 except:
     print """WARNING!!!
 DB info file not found or contains errors! If you are new, run this file with -h flag.
-Make sure you create a file "db_info.py" in root folder of this file.
+Make sure you create a file "%s" in root folder of this file.
 This must contain thislike struct:
 
-""", demo_config
+"""%(sys.argv[1]), demo_config
     exit(1)
 #check for config variables
 try:
@@ -65,9 +67,9 @@ try:
     mysql_mangos_realm = db_info.mysql_mangos_realm
 except:
     print """WARNING!!!
-Some variables cannot be found, make sure db_info.py file contains thislike struct:
+Some variables cannot be found, make sure %s file contains this-like struct:
 
-    """, demo_config
+    """%(sys.argv[1]), demo_config
 
 #make queries
 mysql_auth_string = '--password=%s -u %s -h %s'%(mysql_passwd,mysql_username,mysql_host)
@@ -92,7 +94,7 @@ except:
 dprint("Import of WORLD database...", "Import of WORLD database...\n")
 for table_dump in world_dumps_list:
     dprint('', table_dump)
-    res_code = os.system(mysql_import_exec.format(dump=table_dump))
+    res_code = os.system(mysql_import_exec.format(db=mysql_mangos_world, dump=table_dump))
     if res_code > 0:
         print 'Error on importing of the table file "%s", result code: %s'%(table_dump, res_code)
         exit(1)
@@ -103,7 +105,7 @@ dprint(' done', 'Done importing WORLD database')
 dprint("Import of SCRIPTS database...", "Import of SCRIPTS database...\n")
 for table_dump in scripts_dumps_list:
     dprint('', table_dump)
-    res_code = os.system(mysql_import_exec.format(dump=table_dump))
+    res_code = os.system(mysql_import_exec.format(db=mysql_mangos_scripts, dump=table_dump))
     if res_code > 0:
         print 'Error on importing of the table file "%s", result code: %s'%(table_dump, res_code)
         exit(1)
